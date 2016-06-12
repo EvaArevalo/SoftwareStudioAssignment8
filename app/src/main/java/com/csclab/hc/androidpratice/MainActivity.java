c80665e04f3c6720def28af1bed67174da5a78cd
EvaArevalo <93.evar@gmail.com>
2016-06-12 10:04:20 +0800

    Added IP Layout xml file, jumpToLayout() function to Main Activity.java.

--- app/src/main/java/com/csclab/hc/androidpratice/MainActivity.java
+++ app/src/main/java/com/csclab/hc/androidpratice/MainActivity.java
@@ -8,20 +8,28 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.app.Activity;
+
+import java.io.OutputStream;
+import java.net.Socket;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    /** Init Variable for Page 1 **/
+    /** Init Variable for IP page **/
+    EditText inputIP;
+    Button ipSend;
+    String ipAdd = "";
+
+    /** Init Variable for Calculator **/
     EditText inputNumTxt1;
     EditText inputNumTxt2;
-
     Button btnAdd;
     Button btnSub;
     Button btnMult;
     Button btnDiv;
 
-    /** Init Variable for Page 2 **/
+    /** Init Variable for Result**/
     TextView textResult;
-
     Button return_button;
 
     /** Init Variable **/
@@ -31,19 +39,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /** Func() for setup page 1 **/
-        jumpToMainLayout();
+        jumpToIPLayout();
     }
 
-    /** Function for page 1 setup */
+    //Function for IP Page Setup
+    public void jumpToIPLayout(){
+        setContentView(R.layout.ip_page);
+        inputIP = (EditText)findViewById(R.id.edIP);
+        ipSend = (Button)findViewById(R.id.ipButton);
+
+        ipSend.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ipAdd = inputIP.getText().toString();
+                jumpToMainLayout();
+            }
+        });
+    }
+
+    //Function for Calculator Page Setup
     public void jumpToMainLayout() {
-        //TODO: Change layout to activity_main
-        // HINT: setContentView()
         setContentView(R.layout.activity_main);
 
-        //TODO: Find and bind all elements(4 buttons 2 EditTexts)
-        // inputNumTxt1, inputNumTxt2
-        // btnAdd, btnSub, btnMult, btnDiv
         inputNumTxt1 = (EditText) findViewById(R.id.etNum1);
         inputNumTxt2 = (EditText) findViewById(R.id.etNum2);
 
@@ -52,8 +69,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnMult = (Button) findViewById(R.id.btnMult);
         btnDiv = (Button) findViewById(R.id.btnDiv);
 
-        //TODO: Set 4 buttons' listener
-        // HINT: myButton.setOnClickListener(this);
         btnAdd.setOnClickListener(this);
         btnSub.setOnClickListener(this);
         btnMult.setOnClickListener(this);
@@ -109,29 +124,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void jumpToResultLayout(String resultStr){
         setContentView(R.layout.result_page);
 
-        //TODO: Bind return_button and textResult form result view
-        // HINT: findViewById()
-        // HINT: Remember to give type
+        //Bind return_button and textResult form result view
         return_button = (Button) findViewById(R.id.return_button);
         textResult = (TextView) findViewById(R.id.textResult);
 
         if (textResult != null) {
-            //TODO: Set the result text
+            //Set the result text
             textResult.setText(resultStr);
         }
 
         if (return_button != null) {
-            //TODO: prepare button listener for return button
-            // HINT:
-            // mybutton.setOnClickListener(new View.OnClickListener(){
-            //      public void onClick(View v) {
-            //          // Something to do..
-            //      }
-            // }
+            // prepare button listener for return button
             return_button.setOnClickListener(new View.OnClickListener(){
                 public void onClick(View v) {
-                    // TODO
-                    jumpToMainLayout();
+                    jumpToIPLayout();
                 }
 
             });
